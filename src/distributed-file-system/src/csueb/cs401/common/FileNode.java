cb52cca499751658f64c25cf48b6e093dcb5e2ff
Michael Vu <michaelvu@Michaels-MacBook-Air-2.local>
2022-04-21 14:35:35 -0700

    implemented FileNode

--- src/distributed-file-system/src/csueb/cs401/common/FileNode.java
+++ src/distributed-file-system/src/csueb/cs401/common/FileNode.java
@@ -1,5 +1,41 @@
 package csueb.cs401.common;
 
-public class FileNode {
+import java.util.Date;
 
+public class FileNode {
+	
+	private Date created;
+	private byte[] content;
+	private String owner;
+	private String nodeOwner;
+	
+	/*
+	 * Getters and Setters
+	 */
+	
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public byte[] getContent() {
+		return content;
+	}
+	public void setContent(byte[] content) {
+		this.content = content;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public String getNodeOwner() {
+		return nodeOwner;
+	}
+	public void setNodeOwner(String nodeOwner) {
+		this.nodeOwner = nodeOwner;
+	}
+	
 }
