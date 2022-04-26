6159453637f72fec6aaf73faa895ef82f4c98f63
bryangra <41131495+bryangra@users.noreply.github.com>
2022-04-26 10:57:34 -0700

    Update Logger.java

--- src/distributed-file-system/src/csueb/cs401/persist/Logger.java
+++ src/distributed-file-system/src/csueb/cs401/persist/Logger.java
@@ -1,5 +1,35 @@
 package csueb.cs401.persist;
 
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import csueb.cs401.common.Event;
+
 public class Logger {
+	
+		private List<Event> events = new LinkedList<>();
+		
+		private void saveList() {
+			
+		}
+
+		public void addEvent(String inDescription, String inUser) {
+			
+		}
+		
+		public String findEvents(String startDate, String startTime, String endDate, String endTime) {
+			String foundEvents = new String();
+			
+			return foundEvents;
+		}
+		
+		public void info(String inServerEvent) {
+			
+		}
+		
+		public void warning(String inWarning) {
+			
+		}
 
 }
