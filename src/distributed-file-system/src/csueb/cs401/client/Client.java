d6557dba743155f041bc9b59660b24a1d9a7992c
Andrew <anowinski21@gmail.com>
2022-04-29 00:01:24 -0700

    Changed client

--- src/distributed-file-system/src/csueb/cs401/client/Client.java
+++ src/distributed-file-system/src/csueb/cs401/client/Client.java
@@ -1,63 +1,97 @@
+package csueb.cs401.client;
+
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.util.logging.Logger;
+
+import csueb.cs401.common.Message;
+import csueb.cs401.server.ClientHandler;
+import csueb.cs401.server.Server;
 
 public class Client {
 
+	private static Client me;
+	
+	private Logger LOGGER;
+	
     private Socket socket;
     private ObjectOutputStream oos;
     private ObjectInputStream ois;
     private int port;
-    private String host;
+    
+private Client() {}
+	
+	public static Client getInstance() {
+		if (me == null) {
+			me = new Client();
+			me.init();
+		}
+		return me;
+	}
+	
+	private void init() {
+		
+		// Testing
+		System.out.println("PORT INITIALIZED");
+		
+		// init port
+		if (System.getenv("port") != null) {
+			port = Integer.valueOf(System.getenv("port"));
+		} else {
+			port = 8080;
+		}
+		// init logger
+		System.setProperty("java.util.logging.SimpleFormatter.format",
+	              "[%1$tF %1$tT] [%4$-7s] %5$s %n");
+		LOGGER = Logger.getLogger(Client.class.getName());
+	}
+	
+	public void start() {
+		
+		// Testing
+		System.out.println("START RUNNING");
+		
+		Message msg = new Message();
+		try {
+			socket = new Socket("localhost", port);
+			//LOGGER.info("Server now accepting requests on port " + port);
+			//socket.setReuseAddress(true);
+			
+			// infinite loop for getting client requests
+			while (true) {
+				
+				sendObj(msg);
 
-    private void init() {
-        port = 8080;
-        host = "localhost";
-    }
-
-    public void start() {
-        // Continue until 'logout' entered
-        boolean cont = true;
-
-        // initialze port and host variables
-        init();
-
-        // Continue until logout is entered
-        while(cont) {
-
-            try {
-                socket = new Socket(host, port);
-            } catch (UnknownHostException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-
-        }
-
-        // Server is closed
-
-    }
-
-    public void sendObj() {
+			}
+		} catch (IOException e) {
+			LOGGER.warning(e.getLocalizedMessage());
+		}
+	}
+    
+    public void sendObj(Message msg) throws IOException {
 
         oos = new ObjectOutputStream(socket.getOutputStream());
 
         // Send obj to server
         // Needs completion
-        oos.writeObject(/*obj*/);
+        System.out.println("sending msg");
+        oos.writeObject(msg);
 
     }
 
-    public void receiveObj() {
+    public void receiveObj(Message msg) throws IOException {
 
         ois = new ObjectInputStream(socket.getInputStream());
 
         // Receive object from server
         // Needs completion
-        /*obj*/ = (Message) ois.readObject();
+        try {
+			msg = (Message) ois.readObject();
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
     }
 
