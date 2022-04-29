d6f4d33c3aa72485e9f0e6f41505c59c89475d3c
Andrew <anowinski21@gmail.com>
2022-04-29 00:08:17 -0700

    Added clientStart class

---
+++ src/distributed-file-system/src/csueb/cs401/client/ClientStart.java
@@ -0,0 +1,12 @@
+package csueb.cs401.client;
+
+//import csueb.cs401.client.Client;
+
+public class ClientStart {
+
+	public static void main(String[] args) {
+		Client client = Client.getInstance();
+		client.start();
+	}
+	
+}
